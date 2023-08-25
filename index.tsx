8fdc10907af255e563e96f0b6126c427edab6e4c
cesarriello <cesar.riello@gmail.com>
2023-08-25 09:55:28 -0300

    feat: add todo files

--- index.tsx
+++ index.tsx
@@ -1,8 +1,38 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Welcome to To DO with Bun!");
-  },
-});
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
 
-console.log(`Listening on localhost:${server.port}`);
+const BaseHtml = ({ children }: elements.Children) => `
+  <!DOCTYPE html>
+  <html dir="ltr" lang="pt-br" class="">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Elysia</title>
+
+        <script src="https://unpkg.com/htmx.org@1.9.4"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    ${children}
+  </html>
+`;
+
+const server = new Elysia()
+  .use(html)
+  .get("/", ({ html }) =>
+    html(
+      <BaseHtml>
+        <body class="flex w-full h-screem justify-center items-center">
+          <button hx-post="/api" hx-swap="outerHTML">
+            Clica aqui
+          </button>
+        </body>
+      </BaseHtml>
+    )
+  )
+  .post("/api", () => <div class="text-blue-600">Olá mundo</div>)
+  .listen(3000);
+
+console.log(
+  "I am alive on port 3000",
+  `http://${server.server?.hostname}:${server.server?.port}`
+);
